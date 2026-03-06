03f805b73f954c78fb50d073457586080c424406
Rezial011 <dafaalfarezi27@gmail.com>
2026-03-06 13:32:20 +0700

    path fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {HashRouter, BrowserRouter, Route, Routes } from "react-router-dom";
 import SeoSite from "./Components/seo-site/SeoSite.jsx";
 import "./index.css";
 // import './app.css'
@@ -8,11 +8,11 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/seo-site" element={<SeoSite />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
